c719900a1b7dc2b2d64712943e811a97051f9bfa
antook1 <a.capone10@studenti.unisannio.it>
2024-10-04 16:33:08 +0200

    sono Ale dal pc di anto, ho modificato un commento

--- src/main/java/com/example/fitfuture/control/UserController.java
+++ src/main/java/com/example/fitfuture/control/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+//ciao modifico cose
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
