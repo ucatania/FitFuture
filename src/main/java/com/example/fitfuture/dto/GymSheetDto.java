d56ba8ad7727a730de5b922c0a5f8e636ca819ab
gabriele <163521818+gcitrigno@users.noreply.github.com>
2025-02-18 14:43:19 +0100

    aggiornato

--- src/main/java/com/example/fitfuture/dto/GymSheetDto.java
+++ src/main/java/com/example/fitfuture/dto/GymSheetDto.java
@@ -1,45 +1,45 @@
-package com.example.fitfuture.dto;
-import com.fasterxml.jackson.annotation.JsonProperty;
+    package com.example.fitfuture.dto;
+    import com.fasterxml.jackson.annotation.JsonProperty;
 
-import java.util.List;
+    import java.util.List;
 
-public class GymSheetDto {
-    private String athleteId;
-    private String personalTrainerId;
+    public class GymSheetDto {
+        private String athleteId;
+        private String personalTrainerId;
 
-    @JsonProperty("exerciseIds")
-    private List<String> exerciseIds;
+        @JsonProperty("exerciseIds")
+        private List<String> exerciseIds;
 
-    public GymSheetDto(String athleteId, String personalTrainerId, List<String> exerciseIds) {
-        this.athleteId = athleteId;
-        this.personalTrainerId = personalTrainerId;
-        this.exerciseIds = exerciseIds;
+        public GymSheetDto(String athleteId, String personalTrainerId, List<String> exerciseIds) {
+            this.athleteId = athleteId;
+            this.personalTrainerId = personalTrainerId;
+            this.exerciseIds = exerciseIds;
+        }
+
+        public GymSheetDto(){
+        }
+
+        public String getAthleteId() {
+            return athleteId;
+        }
+
+        public void setAthleteId(String athleteId) {
+            this.athleteId = athleteId;
+        }
+
+        public String getPersonalTrainerId() {
+            return personalTrainerId;
+        }
+
+        public void setPersonalTrainerId(String personalTrainerId) {
+            this.personalTrainerId = personalTrainerId;
+        }
+
+        public List<String> getExerciseIds() {
+            return exerciseIds;
+        }
+
+        public void setExerciseIds(List<String> exerciseIds) {
+            this.exerciseIds = exerciseIds;
+        }
     }
-
-    public GymSheetDto(){
-    }
-
-    public String getAthleteId() {
-        return athleteId;
-    }
-
-    public void setAthleteId(String athleteId) {
-        this.athleteId = athleteId;
-    }
-
-    public String getPersonalTrainerId() {
-        return personalTrainerId;
-    }
-
-    public void setPersonalTrainerId(String personalTrainerId) {
-        this.personalTrainerId = personalTrainerId;
-    }
-
-    public List<String> getExerciseIds() {
-        return exerciseIds;
-    }
-
-    public void setExerciseIds(List<String> exerciseIds) {
-        this.exerciseIds = exerciseIds;
-    }
-}
